c4dd3cb3754255b3638d04cbb741686cf0e0fde5
Alfred Reyes <john.alfred@homeqube.com>
2024-09-25 21:55:06 +0800

    feat: added markdown component to properly display streamed value

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
+import Markdown from 'react-markdown'
 import { Container, Stack, TextField, Button, Box } from '@mui/material';
 import './App.css'
 
 
 function App() {
-  const [ prompt, setPrompt ] = useState("")
+  const [ question, setQuestion ] = useState("")
   const [ loading, setLoading ] = useState(false)
   const [ streamedAnswer, setStreamedAnswer ] = useState("")
 
+  const handleClick = async e => {
+    e.preventDefault()
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/ask`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ question }), // server is expecting JSON
+    });
+
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
+    const loopRunner = true;
+
+    while (loopRunner) {
+      const { value, done } = await reader.read();
+      if (done) {
+        break;
+      }
+      const decodedChunk = decoder.decode(value, { stream: true });
+      setStreamedAnswer((answer) => answer + decodedChunk);
+    }
+  }
   return (
     <Container
       sx={{
@@ -27,6 +51,7 @@ function App() {
           label="Enter your question"
           variant="outlined"
           margin="normal"
+          value={question}
           sx={{
             '& .MuiOutlinedInput-root': {
               '& fieldset': {
@@ -34,8 +59,10 @@ function App() {
               },
             },
             width: "80vw",
-            borderRadius: "20px"
+            borderRadius: "20px",
+            outline: "none"
           }}
+          onChange={e => setQuestion(e.target.value)}
         />
         <Stack
           flexDirection="row"
@@ -52,9 +79,13 @@ function App() {
             variant="contained"
             color="primary"
             sx={{ mt: 2 }}
+            onClick={e => handleClick(e)}
           >
             Ask
           </Button>
+        </Stack>
+        <Stack mt={7}>
+          <Markdown>{streamedAnswer}</Markdown>
         </Stack>
       </Box>
     </Container>
