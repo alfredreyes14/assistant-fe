38848da1593321bec2f93258d9783455cb4c77ae
Alfred Reyes <john.alfred@homeqube.com>
2024-09-25 18:27:44 +0800

    feat: basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
 import { useState } from 'react'
-import Button from '@mui/material/Button';
+import { Container, Stack, TextField, Button, Box } from '@mui/material';
+import './App.css'
+
 
 function App() {
   const [ prompt, setPrompt ] = useState("")
   const [ loading, setLoading ] = useState(false)
   const [ streamedAnswer, setStreamedAnswer ] = useState("")
+
   return (
-    <>
-      <Button variant="contained">Hello world</Button>
-    </>
+    <Container
+      sx={{
+        width: "100%"
+      }}
+      disableGutters={true}
+      maxWidth="lg"
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        minHeight="100vh"
+      >
+        <TextField
+          label="Enter your question"
+          variant="outlined"
+          margin="normal"
+          sx={{
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderRadius: '15px', // Adjust the value as needed
+              },
+            },
+            width: "80vw",
+            borderRadius: "20px"
+          }}
+        />
+        <Stack
+          flexDirection="row"
+          gap={3}
+        >
+          <Button
+            variant="text"
+            color="primary"
+            sx={{ mt: 2 }}
+          >
+            Clear
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ mt: 2 }}
+          >
+            Ask
+          </Button>
+        </Stack>
+      </Box>
+    </Container>
   )
 }
 
